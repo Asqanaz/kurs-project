d422127ee59cde592b913d4b6c78e59e35644ef9
Asqanaz <asmoyan.04@gmail.com>
2022-12-04 18:32:33 +0400

    update

--- src/components/forgotpassword/PasswordEdit.jsx
+++ src/components/forgotpassword/PasswordEdit.jsx
@@ -1,85 +1,123 @@
-import React, { useEffect, useState } from "react";
-import { useForm } from "react-hook-form";
-import { Link } from "react-router-dom";
+import React, {useEffect, useState} from "react"
+import {useForm} from "react-hook-form"
+import {Link} from "react-router-dom"
+import TextField from "@mui/material/TextField"
+import Box from "@mui/material/Box"
+import FormHelperText from "@mui/material/FormHelperText"
 
-export const PasswordEdit = ({ userDatas, setUserDatas }) => {
-  const {
-    register,
-    formState: { isValid, errors },
-    handleSubmit,
-    reset
-  } = useForm({ mode: "onSubmit", reValidateMode: "onSubmit" });
+export const PasswordEdit = ({userDatas, setUserDatas}) => {
+	const {
+		register,
+		formState: {isValid, errors},
+		handleSubmit,
+		reset
+	} = useForm({mode: "onSubmit", reValidateMode: "onSubmit"})
 
-  const handleChangeDatas = (data) => {
-    if (isValid) {
-      setUserDatas({
-        username: data.newUserName,
-        password: data.newPassword,
-      });
-      reset()
-    }
+	const handleChangeDatas = data => {
+		if (isValid) {
+			setUserDatas({
+				username: data.newUserName,
+				password: data.newPassword
+			})
+			reset()
+		}
 
-    reset();
-  };
+		reset()
+	}
 
-  console.log(errors);
-  return (
-    <form className="form-container" onSubmit={handleSubmit(handleChangeDatas)}>
-      <div className="form-fields">
-        <span>Current UserName</span>
-        <input
-          type="text"
-          {...register("currentUserName", {
-            require: "Required field",
-            validate: (value) => {
-              if (value !== userDatas.username) {
-                return "UserDatas doesn't match";
-              }
-              return true;
-            },
-          })}
-        />
-      </div>
-      <div className="form-fields">
-        <span>Current Password</span>
-        <input
-          type="text"
-          {...register("currentPassword", {
-            require: "Required field",
-            validate: (value) => {
-              if (value !== userDatas.password) {
-                return "UserDatas doesn't match";
-              }
-              return true;
-            }
-          })}
-        />
-      </div>
-      <div className="form-fields">
-        <span>New UserName</span>
-        <input
-          type="text"
-          {...register("newUserName", {
-            require: "Required field",
-          })}
-        />
-      </div>
-      <div className="form-fields">
-        <span>New Password</span>
-        <input
-          type="text"
-          {...register("newPassword", {
-            require: "Required field",
-          })}
-        />
-      </div>
-      <div className="back-to-login">
-        <p>
-          {errors?.currentPassword?.message || errors?.currentUserName?.message ? errors.currentPassword.message : "Datas successfully changed"}
-        </p>
-        <Link to="/login">Back to login</Link>
-        <button type="submit">Edit Datas</button>
-      </div>
-    </form>
-  );
-};
+
+	console.log(errors)
+	return (
+		<form className="form-container flex flex-col" onSubmit={handleSubmit(handleChangeDatas)}>
+			<Box
+				sx={{
+					display: "flex",
+					alignItems: "flex-end",
+					marginTop: "1rem",
+					justifyContent: "space-around",
+					width: 100 + "%"
+				}}
+			>
+				<TextField
+					label="Current User Name"
+					variant="standard"
+					{...register("currentUserName", {
+						required: "Required field",
+						validate: value => {
+							if (userDatas.username !== value) {
+								return "User datas doesn't match"
+							}
+
+							return true
+						}
+					})}
+				/>
+			</Box>
+			<Box
+				sx={{
+					display: "flex",
+					alignItems: "flex-end",
+					marginTop: "1rem",
+					justifyContent: "space-around",
+					width: 100 + "%"
+				}}
+			>
+				<TextField
+					label="Current Password"
+					variant="standard"
+					{...register("currentPassword", {
+						required: "Required field",
+						validate: value => {
+							if (userDatas.password !== value) {
+								return "User datas doesn't match"
+							}
+
+							return true
+						}
+					})}
+				/>
+			</Box>
+			<Box
+				sx={{
+					display: "flex",
+					alignItems: "flex-end",
+					marginTop: "1rem",
+					justifyContent: "space-around",
+					width: 100 + "%"
+				}}
+			>
+				<TextField
+					label="New User Name"
+					variant="standard"
+					{...register("newUserName", {
+						required: "Required field"
+					})}
+				/>
+			</Box>
+			<Box
+				sx={{
+					display: "flex",
+					alignItems: "flex-end",
+					marginTop: "1rem",
+					justifyContent: "space-around",
+					width: 100 + "%"
+				}}
+			>
+				<TextField
+					label="New Password"
+					variant="standard"
+					{...register("newPassword", {
+						required: "Required field"
+					})}
+				/>
+			</Box>
+			<div className="back-to-login flex flex-col justify-center items-center mt-6">
+				<FormHelperText>{errors?.currentUserName?.message || errors?.currentPassword?.message || "Data"}</FormHelperText>
+				<Link to="/login">Back to login</Link>
+				<button type="submit" className="mt-5">
+					Edit Datas
+				</button>
+			</div>
+		</form>
+	)
+}
--- src/components/loginpage/Login.jsx
+++ src/components/loginpage/Login.jsx
@@ -1,69 +1,88 @@
-import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
-import { useForm } from "react-hook-form";
-import TextField from "@mui/material/TextField";
-import { MdPassword } from "react-icons/md";
-import { AiOutlineUser } from "react-icons/ai";
-import Box from "@mui/material/Box";
-import FormHelperText from '@mui/material/FormHelperText';
+import React, {useState, useEffect} from "react"
+import {Link} from "react-router-dom"
+import {useForm} from "react-hook-form"
+import TextField from "@mui/material/TextField"
+import {MdPassword} from "react-icons/md"
+import {AiOutlineUser} from "react-icons/ai"
+import Box from "@mui/material/Box"
+import FormHelperText from "@mui/material/FormHelperText"
 
-export const Login = ({ userDatas }) => {
-  const {
-    register,
-    formState: { isValid, errors },
-    handleSubmit,
-  } = useForm({ mode: "onSubmit", reValidateMode: "onSubmit" });
+export const Login = ({userDatas}) => {
+	const {
+		register,
+		formState: {errors},
+		handleSubmit
+	} = useForm({mode: "onSubmit", reValidateMode: "onSubmit"})
 
-  const handleOnSubmit = (data) => {
-    console.log(data);
-  };
+	const handleOnSubmit = data => {
+		console.log(data)
+	}
 
-  console.log(errors);
-  return (
-    <form
-      className="flex justify-center items-center flex-col container h-[50vh] w-80 form-container"
-      onSubmit={handleSubmit(handleOnSubmit)}
-    >
-      <h2 className="font-bold text-4xl text-center">Login</h2>
-      <Box sx={{ display: "flex", alignItems: "flex-end", marginTop: "1rem", justifyContent: "space-around", width: 100 + "%" }}>
-        <AiOutlineUser size={24} />
-        <TextField
-          label="User Name"
-          variant="standard"
-          {...register("login", {
-            required: "Required field",
-            validate: (value) => {
-              if (userDatas.username !== value) {
-                return "User datas doesn't match";
-              }
+	console.log(errors)
+	return (
+		<form
+			className="flex justify-center items-center flex-col container h-[50vh] w-80 form-container"
+			onSubmit={handleSubmit(handleOnSubmit)}
+		>
+			<h2 className="font-bold text-4xl text-center">Login</h2>
+			<Box
+				sx={{
+					display: "flex",
+					alignItems: "flex-end",
+					marginTop: "1rem",
+					justifyContent: "space-around",
+					width: 100 + "%"
+				}}
+			>
+				<AiOutlineUser size={24} />
+				<TextField
+					label="User Name"
+					variant="standard"
+					{...register("login", {
+						required: "Required field",
+						validate: value => {
+							if (userDatas.username !== value) {
+								return "User datas doesn't match"
+							}
 
-              return true;
-            },
-          })}
-          sx = {{'&:focus': {backgroundColor: "green"}}}
-        />
-      </Box>
-      <Box sx={{ display: "flex", alignItems: "flex-end", marginTop: "1rem", justifyContent: "space-around", width: 100 + "%"}}>
-        <MdPassword size={24} />
-        <TextField
-          id="input-with-sx"
-          label="Password"
-          variant="standard"
-          {...register("password", {
-            required: "Required field",
-            validate: (value) => {
-              if (userDatas.password !== value) {
-                return "User datas doesn't match";
-              }
-              return true;
-            },
-          })}
-        />
-      </Box>
-      <FormHelperText sx = {{color: 'red', marginTop: 2 + "rem"}}>{errors?.login?.message}</FormHelperText>
-      <Link to="/forgot-password" className="mt-3">Forgot password?</Link>
-      <button className="w-full h-10 rounded-2xl border hover:border-violet-600 bg-white-600 text-grey-500 mt-5">Submit</button>
-        
-    </form>
-  );
-};
+							return true
+						}
+					})}
+				/>
+			</Box>
+			<Box
+				sx={{
+					display: "flex",
+					alignItems: "flex-end",
+					marginTop: 1 + "rem",
+					justifyContent: "space-around",
+					width: 100 + "%"
+				}}
+			>
+				<MdPassword size={24} />
+				<TextField
+					label="Password"
+					variant="standard"
+					{...register("password", {
+						required: "Required field",
+						validate: value => {
+							if (userDatas.password !== value) {
+								return "User datas doesn't match"
+							}
+							return true
+						}
+					})}
+				/>
+			</Box>
+			<FormHelperText sx={{color: "red", marginTop: 1 + "rem"}}>
+				{errors?.login?.message || errors?.password?.message}
+			</FormHelperText>
+			<Link to="/forgot-password" className="mt-6">
+				Forgot password?
+			</Link>
+			<button className="w-full h-10 rounded-2xl border hover:border-violet-600 bg-white-600 text-grey-500 mt-5">
+				Submit
+			</button>
+		</form>
+	)
+}
